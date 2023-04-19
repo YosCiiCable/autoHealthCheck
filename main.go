64bd85f266292200c3995de4d7e2301d1cf4b3a0
YosCiiCable <YosCiiCable@gmail.com>
2023-04-19 14:31:26 +0900

    first commit🌱

---
+++ main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	//"strconv"
+	//"time"
+
+	"github.com/chromedp/cdproto/input"
+	//"github.com/chromedp/cdproto/target"
+	"github.com/chromedp/chromedp"
+
+	"github.com/manifoldco/promptui"
+)
+
+func main() {
+	// 上位行をコメントアウトすることで下位行を有効化できます
+
+	// level0: chromeのインスタンス作成
+	//ctx, _ := chromedp.NewContext(context.Background()) /*
+	// level0-debug1: ログあり でインスタンス作成
+	//ctx, _ := chromedp.NewContext(context.Background(), chromedp.WithDebugf(log.Printf)) /*
+	// level0-debug2: no headless でインスタンス作成
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.Flag("headless", false),
+		chromedp.Flag("enable-automation", false),
+	)
+	allocCtx, _ := chromedp.NewExecAllocator(context.Background(), opts...)
+	ctx, _ := chromedp.NewContext(allocCtx, chromedp.WithLogf(log.Printf))
+	//*/
+	//defer cancel()
+
+	if err := chromedp.Run(ctx, chromedp.Navigate("https://forms.gle/2iPTW6X4XjHCu4ar7")); err != nil {
+		log.Fatal("err1: Failed login")
+	}
+
+	// level1: login
+	fmt.Println("Ctrl+C で強制停止できます(Windows)。全く動かない時など、お試しあれ。")
+	login(ctx, "sを含めた学籍番号: ", "@ga.ariake-nct.ac.jp")
+
+	// level2: 遷移チェック
+	/* fmt.Println("ロード中………")
+	if err := chromedp.Run(ctx,
+		chromedp.Click(`//*[@id="LbtMaRePassword"]/font`, chromedp.AtLeast(0)),
+	); err == nil {
+		log.Fatal("err2: Transition check fails")
+	}
+	fmt.Println("ロード完了")
+	*/
+}
+
+func login(ctx context.Context, inputMessage string, additionalInfo string) {
+	var credential string
+	fmt.Scan(&credential)
+	credential = credential + additionalInfo
+	fmt.Println("ログイン処理開始………")
+
+	if err := chromedp.Run(ctx,
+		chromedp.Click(`//*[@id="identifierId"]`, chromedp.NodeVisible),
+		input.InsertText(credential),
+	); err != nil {
+		log.Fatal("err1: Failed login")
+	}
+
+	fmt.Println("ログイン完了")
+}
+
+func passwdInputer(labelMessage string) string {
+	fmt.Println("赤色の x が付く場合がありますが、気にしないでください。")
+	validate := func(input string) error {
+		return nil
+	}
+
+	prompt := promptui.Prompt{
+		Label:    labelMessage,
+		Validate: validate,
+		Mask:     '*',
+	}
+
+	passwd, err := prompt.Run()
+	if err != nil {
+		log.Fatal("err@passwdInputer: Failed to run prompt")
+	}
+
+	return passwd
+}
+
+func debugURL(ctx context.Context) {
+	var url string
+	if err := chromedp.Run(ctx,
+		chromedp.Location(&url),
+	); err != nil {
+		log.Fatal("err@debugURL: Failed to location url")
+	}
+	fmt.Printf("debugURL: %s\n", url)
+}
+
+func debugPic(ctx context.Context) {
+	var buf []byte
+	// debug level1: スクショ撮影
+	if err := chromedp.Run(ctx,
+		chromedp.FullScreenshot(&buf, 90),
+	); err != nil {
+		log.Fatal("err@debugPic-1: Failed to capture a screenshot")
+	}
+	// debug level2: スクショ出力
+	if err := os.WriteFile(
+		"fullScreenshot.png", buf, 0o644,
+	); err != nil {
+		log.Fatal("err@debugPic-2: Failed to output the screenshot")
+	}
+}
